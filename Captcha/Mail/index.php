54d1e44d966d1ad040ce726edeb7eb76c1f4690b
Erhai_lake <fuzixuan0714_0826@163.com>
2024-08-06 18:28:20 +0800

    3.1.2
    邮箱验证码
    增加:
    样式3
    
    修改:
    缩进修改
    增加收信人邮箱的插销
    样式自动识别样式数量

--- Captcha/Mail/index.php
+++ Captcha/Mail/index.php
@@ -8,68 +8,74 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 if ($Auth->Authenticate()) {
-  // SMTP服务器
-  $Host = (string)$Auth->StringParameters('Host');
-  // SMTP身份验证
-  $SMTPAuth = (int)$Auth->RangeIntParameters('SMTPAuth', 1, 2, 1);
-  // SMTP用户名
-  $Name = (string)$Auth->StringParameters('Name');
-  // SMTP密码
-  $Password = (string)$Auth->StringParameters('Password');
-  // SMTP端口
-  $Port = (int)$Auth->IntParameters('Port');
-  // 发信人邮箱地址
-  $SenderEmail = (string)$Auth->StringParameters('SenderEmail');
-  // 发信人名称
-  $SenderName = (string)$Auth->StringParameters('SenderName');
-  // 收信人邮箱地址
-  $ReceiversEmail = (string)$Auth->StringParameters('ReceiversEmail');
-  // 收信人名称
-  $ReceiversName = (string)$Auth->StringParameters('ReceiversName');
-  // 平台
-  $Platform = (string)$Auth->StringParameters('Platform');
-  // 平台Logo
-  $PlatformLogo = (string)$Auth->StringParameters('PlatformLogo');
-  // 操作
-  $Operation = (string)$Auth->StringParameters('Operation');
-  // 样式
-  $Style = (int)$Auth->RangeIntParameters('Style', 1, 2, 1);
+    // SMTP服务器
+    $Host = (string)$Auth->StringParameters('Host');
+    // SMTP身份验证
+    $SMTPAuth = (int)$Auth->RangeIntParameters('SMTPAuth', 1, 2, 1);
+    // SMTP用户名
+    $Name = (string)$Auth->StringParameters('Name');
+    // SMTP密码
+    $Password = (string)$Auth->StringParameters('Password');
+    // SMTP端口
+    $Port = (int)$Auth->IntParameters('Port');
+    // 发信人邮箱地址
+    $SenderEmail = (string)$Auth->StringParameters('SenderEmail');
+    // 发信人名称
+    $SenderName = (string)$Auth->StringParameters('SenderName');
+    // 收信人邮箱地址
+    $ReceiversEmail = (string)$Auth->StringParameters('ReceiversEmail');
+    // 收信人名称
+    $ReceiversName = (string)$Auth->StringParameters('ReceiversName');
+    // 平台
+    $Platform = (string)$Auth->StringParameters('Platform');
+    // 平台Logo
+    $PlatformLogo = (string)$Auth->StringParameters('PlatformLogo');
+    // 操作
+    $Operation = (string)$Auth->StringParameters('Operation');
+    // 样式
+    $Style = (int)$Auth->RangeIntParameters('Style', 1, count(glob('Style/*.html')), 1);
 }
 
 if ($ValidRequest) {
-  $Mail = new PHPMailer(true);
-  try {
-    $Mail->isSMTP();
-    $Mail->Host = $Host;
-    if ($SMTPAuth === 1) {
-      $Mail->SMTPAuth = true;
-    } else {
-      $Mail->SMTPAuth = false;
+    $Mail = new PHPMailer(true);
+    try {
+        $Mail->isSMTP();
+        $Mail->Host = $Host;
+        if ($SMTPAuth === 1) {
+            $Mail->SMTPAuth = true;
+        } else {
+            $Mail->SMTPAuth = false;
+        }
+        $Mail->Username = $Name;
+        $Mail->Password = $Password;
+        $Mail->SMTPSecure = 'tls';
+        $Mail->Port = $Port;
+        $Mail->CharSet = 'UTF-8';
+        $Mail->Encoding = 'base64';
+        $Mail->setFrom($SenderEmail, $SenderName);
+        $Mail->addAddress($ReceiversEmail, $ReceiversName);
+        $Mail->Subject = $Platform;
+        $RandomText = substr(str_shuffle("01TDIJKLGHU23NOR6SE78PQ9AB4MF5CVWXYZ"), 0, 6);
+        $ReplaceArray = [
+            // 平台
+            '{{Platform}}' => $Platform,
+            // 平台Logo
+            '{{PlatformLogo}}' => $PlatformLogo,
+            // 验证码
+            '{{Captcha}}' => $RandomText,
+            // 操作
+            '{{Operation}}' => $Operation,
+            // 收信人邮箱
+            '{{ReceiversEmail}}' => $ReceiversEmail
+        ];
+        $HtmlContent = strtr(file_get_contents('Style/' . $Style . '.html'), $ReplaceArray);
+        $Mail->isHTML(true);
+        $Mail->Body = $HtmlContent;
+        $Mail->send();
+        $Response['Data'] = Argon2Encipher($RandomText);
+    } catch (Exception $E) {
+        $Auth->Custom('邮件发送失败');
     }
-    $Mail->Username = $Name;
-    $Mail->Password = $Password;
-    $Mail->SMTPSecure = 'tls';
-    $Mail->Port = $Port;
-    $Mail->CharSet = 'UTF-8';
-    $Mail->Encoding = 'base64';
-    $Mail->setFrom($SenderEmail, $SenderName);
-    $Mail->addAddress($ReceiversEmail, $ReceiversName);
-    $Mail->Subject = $Platform;
-    $RandomText = substr(str_shuffle("01TDIJKLGHU23NOR6SE78PQ9AB4MF5CVWXYZ"), 0, 6);
-    $ReplaceArray = [
-      '{{Platform}}' => $Platform,
-      '{{PlatformLogo}}' => $PlatformLogo,
-      '{{Captcha}}' => $RandomText,
-      '{{Operation}}' => $Operation
-    ];
-    $HtmlContent = strtr(file_get_contents('Style/' . $Style . '.html'), $ReplaceArray);
-    $Mail->isHTML(true);
-    $Mail->Body = $HtmlContent;
-    $Mail->send();
-    $Response['Data'] = Argon2Encipher($RandomText);
-  } catch (Exception $E) {
-    $Auth->Custom('邮件发送失败');
-  }
 }
 
 $Auth->End();
@@ -79,10 +85,10 @@ echo json_encode($Response);
 
 function Argon2Encipher(string $Plaintext)
 {
-  $Options = [
-    'memory_cost' => 65536,
-    'time_cost' => 10,
-    'threads' => 2
-  ];
-  return base64_encode(password_hash($Plaintext, PASSWORD_ARGON2I, $Options));
+    $Options = [
+        'memory_cost' => 65536,
+        'time_cost' => 10,
+        'threads' => 2
+    ];
+    return base64_encode(password_hash($Plaintext, PASSWORD_ARGON2I, $Options));
 }
